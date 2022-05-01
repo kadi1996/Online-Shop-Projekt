1cfabab34c81be14ec13bb79f26545cebc3a08cc
Kadi <gueneykadircan@gmail.com>
2022-05-01 21:42:07 +0200

    ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-console.log("text");
+// console.log("text");
+
+let slideInMenu = document.getElementById('slide-in-menu');
+
+function showMenu() {
+    console.log("show");
+    slideInMenu.style.left = 0;
+    slideInMenu.style.right = 0;
+}
